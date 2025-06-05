e59920f4c8afead86727f6fc8ed4c54c09ed1cb8
Erick Ramon Hernandez Ortega <erickramon47@live.com.mx>
2025-06-04 18:44:19 -0600

    feat: configurar server

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,5 +1,13 @@
+import {CronService} from "./cron/cron-service";
+import {CheckService} from "../domain/use-cases/checks/check-service";
+
 export class ServerApp {
     public static start(): void {
-        console.log('Server started');
+		CronService.createJob('*/5 * * * * *', (): void => {
+            new CheckService(
+                (): void => console.log('url is ok'),
+                (error: string): void => console.error(error),
+            ).execute('https://www.google.com/')
+        });
     }
 }
