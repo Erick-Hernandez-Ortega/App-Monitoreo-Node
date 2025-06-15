587a9f5129c46cf4fcf6d779a3dde78982b3fd53
Erick Ramon Hernandez Ortega <erickramon47@live.com.mx>
2025-06-15 16:06:11 -0600

    feat: agregar LogRepositoryImplementation a la instancia

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,5 +1,9 @@
 import {CronService} from "./cron/cron-service";
 import {CheckService} from "../domain/use-cases/checks/check-service";
+import {LogRepositoryImplementation} from "../infrastructure/repositories/log.repository";
+import {FileSystemDatasource} from "../infrastructure/datasources/file-system.datasource";
+
+const fileSystemLogRepository: LogRepositoryImplementation = new LogRepositoryImplementation(new FileSystemDatasource())
 
 export class ServerApp {
     public static start(): void {
@@ -7,6 +11,7 @@ export class ServerApp {
             new CheckService(
                 (): void => console.log('url is ok'),
                 (error: string): void => console.error(error),
+                fileSystemLogRepository,
             ).execute('https://www.google.com/')
         });
     }
