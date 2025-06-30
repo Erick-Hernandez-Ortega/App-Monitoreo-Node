9f025e38ecec3833d621c92f2f5f03d7417364b9
Erick Ramon Hernandez Ortega <erickramon47@live.com.mx>
2025-06-29 19:12:20 -0600

    feat: hacer conexion con mongo

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,15 @@
 import { ServerApp } from "./presentation/server";
+import { MongoDatabase } from "./data/mongo";
+import {envs} from "./config/plugins/envs.plugin";
 
 (async (): Promise<void> => {
-  main();
+  await main();
 })();
 
-function main(): void {
+async function main(): Promise<void> {
+  await MongoDatabase.connect({
+    mongoUrl: envs.MONGO_URL,
+    dbName: envs.MONGO_DB_NAME,
+  });
   ServerApp.start();
 }
